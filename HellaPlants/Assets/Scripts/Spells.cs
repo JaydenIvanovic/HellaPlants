ef7799fd84ed3aa35f5ff6e0c00e768f5531d863
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-03-29 19:54:55 +1100

    Added a script so the player can interact with the game. Instead of gestures just use 1, 2, 3 and 4 on keyboard to see the change in weather. Only sun, fertilizer and rain are implemented so far. These are just visual changes as the dynamic weather system has not yet been implemented. The environment game object has an empty script to start on this.
    Signed-off-by: JaydenIvanovic <s3331571@student.rmit.edu.au>

---
+++ HellaPlants/Assets/Scripts/Spells.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 
+ * Script which handles the user casting spells.
+ * Key presses for now, gesture recognition will come after 
+ * implementation of basic logic.
+ */
+public class Spells : MonoBehaviour 
+{
+    public GameObject rain, fertilizer, wind, sun;
+    private bool rain_i, fertilizer_i, wind_i, sun_i; // keep track of which prefabs have been instantiated.
+
+	// Use this for initialization
+	void Start () 
+    {
+        rain_i = false;
+        fertilizer_i = false;
+        wind_i = false;
+        sun_i = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // Cast sun.
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (!sun_i)
+            {
+                sun_i = true;
+                Instantiate(sun);
+            }
+        }
+        // Cast rain.
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (!rain_i)
+            {
+                rain_i = true;
+                Instantiate(rain);
+            }
+        }
+        // Cast wind.
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ;
+        }
+        // Cast fertilizer.
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Instantiate(fertilizer);
+        }
+    }
+}
