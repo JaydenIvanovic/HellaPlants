04770359fc1c0b295da35c89c2de5c1f6d098b2c
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-04-01 16:31:13 +1100

    Updated player spell casting. Fertilizer can be cast at any time. Sun, rain and wind can be cast at any time to but will override the current one. The max length of those 3 spells can be changed with the MAX_SECONDS variable in Spells.cs.
    Signed-off-by: JaydenIvanovic <s3331571@student.rmit.edu.au>

--- HellaPlants/Assets/Scripts/Spells.cs
+++ HellaPlants/Assets/Scripts/Spells.cs
@@ -9,27 +9,31 @@ using System.Collections;
 public class Spells : MonoBehaviour 
 {
     public GameObject rain, fertilizer, wind, sun;
-    private bool rain_i, fertilizer_i, wind_i, sun_i; // keep track of which prefabs have been instantiated.
-
-	// Use this for initialization
+    public float MAX_SECONDS;
+    private GameObject rain_i, fertilizer_i, wind_i, sun_i;
+    private float secondsPassed;
+	
+    // Use this for initialization
 	void Start () 
     {
-        rain_i = false;
-        fertilizer_i = false;
-        wind_i = false;
-        sun_i = false;
+        secondsPassed = 0f;
+        Reset();
 	}
+
 	
 	// Update is called once per frame
 	void Update () 
     {
+        bool timedSpellInProgress = TimedSpellInProgress();
+        
         // Cast sun.
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             if (!sun_i)
             {
-                sun_i = true;
-                Instantiate(sun);
+                if (timedSpellInProgress)
+                    Reset();
+                sun_i = (GameObject)Instantiate(sun);
             }
         }
         // Cast rain.
@@ -37,19 +41,90 @@ public class Spells : MonoBehaviour
         {
             if (!rain_i)
             {
-                rain_i = true;
-                Instantiate(rain);
+                if (timedSpellInProgress)
+                    Reset();
+                rain_i = (GameObject)Instantiate(rain);
             }
         }
         // Cast wind.
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            ;
+            if (!wind_i)
+            {
+                if (timedSpellInProgress)
+                    Reset();
+                wind_i = (GameObject)Instantiate(wind);
+            }
         }
         // Cast fertilizer.
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            Instantiate(fertilizer);
+            if (!fertilizer_i)
+                fertilizer_i = (GameObject) Instantiate(fertilizer);
         }
+        
+        if(timedSpellInProgress)
+            CheckWeatherTimer();
+    }
+
+    
+    // Reset game state.
+    private void Reset()
+    {
+        if (fertilizer_i)
+            Destroy(fertilizer_i);
+
+        if (rain_i)
+            Destroy(rain_i);
+
+        if (wind_i)
+            Destroy(wind_i);
+
+        if (sun_i)
+            Destroy(sun_i);
+
+        secondsPassed = 0f;
+        fertilizer_i = null;
+        rain_i = null;
+        wind_i = null;
+        sun_i = null;
+    }
+ 
+
+    // Check how long the current weather condition has been
+    // going on for and terminate it if it exceeds the limit.
+    private void CheckWeatherTimer()
+    {
+        if (secondsPassed >= MAX_SECONDS)
+        {
+            Debug.Log(secondsPassed + " -> hit 3 seconds.");
+            secondsPassed = 0;
+            Reset();
+        }
+        else
+        {
+            Debug.Log(secondsPassed);
+            secondsPassed += Time.deltaTime;
+        }
+    }
+
+
+    // Check if a weather condition has been cast and is playing out.
+    // Fertilizer is not included. It doesn't need to be timed it handles 
+    // its own destruction.
+    private bool TimedSpellInProgress()
+    {
+        if (wind_i || rain_i || sun_i)
+            return true;
+        return false;
+    }
+
+
+    // Check if any spell is currently in progress.
+    private bool SpellInProgress()
+    {
+        if (TimedSpellInProgress() || fertilizer_i)
+            return true;
+        return false;
     }
 }
