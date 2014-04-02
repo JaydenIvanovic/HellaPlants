3a611789cd26126ba0953837de86fb8eab9963d0
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-04-02 23:41:05 +1100

    Had some time to make the plants sun, soil and water levels change over time. Its a rough implementation but gives a basic idea.
    Signed-off-by: JaydenIvanovic <s3331571@student.rmit.edu.au>

--- HellaPlants/Assets/Scripts/PlantState.cs
+++ HellaPlants/Assets/Scripts/PlantState.cs
@@ -7,6 +7,7 @@ public class PlantState : MonoBehaviour
     private float soil {get;set;}
     private float water {get;set;}
     private float sun {get;set;}
+    private RandomWeather rw;
 
 	// Use this for initialization
 	void Awake () 
@@ -17,6 +18,12 @@ public class PlantState : MonoBehaviour
 		sun = 100;	
 	}
 	
+    void Start()
+    {
+        GameObject environ = GameObject.FindGameObjectWithTag("Environment");
+        rw = environ.GetComponent<RandomWeather>();
+    }
+
 	// Update is called once per frame
 	void Update () 
 	{
@@ -25,6 +32,24 @@ public class PlantState : MonoBehaviour
 		// Check for pests.
 
 		// Check the weather.
+        if (rw.GetWeather() == RandomWeather.Weather.Sunny)
+        {
+            sun = ValueFilter(sun, 0.08f);
+            water = ValueFilter(water, -0.06f);
+        }
+        else if (rw.GetWeather() == RandomWeather.Weather.Rainy)
+        {
+            sun = ValueFilter(sun, -0.06f);
+            water = ValueFilter(water, 0.08f);
+        }
+        else if (rw.GetWeather() == RandomWeather.Weather.Cloudy)
+        {
+            sun = ValueFilter(sun, -0.09f);
+            water = ValueFilter(water, -0.09f);
+        }
+
+        // Soil always gradually depletes.
+        soil = ValueFilter(soil, -0.03f);
 
         // Show visual update.
         Update3DText();
@@ -40,5 +65,17 @@ public class PlantState : MonoBehaviour
         plantVarTxt.transform.Find("WaterLevelsText").GetComponent<TextMesh>().text = "Water: " + water + "%";
         plantVarTxt.transform.Find("SoilLevelsText").GetComponent<TextMesh>().text = "Soil: " + soil + "%";
         plantVarTxt.transform.Find("SunLevelsText").GetComponent<TextMesh>().text = "Sun: " + sun + "%";
+    }
+
+    // Ensure that the number is always between 0 and 100.
+    private float ValueFilter(float val, float inc)
+    {
+        float deltaVal = val + inc;
+
+        if (deltaVal < 0)
+            return 0;
+        else if (deltaVal > 100)
+            return 100;
+        return deltaVal;
     }
 }
