95ef670273583e299a37cd13e031408b49cf01d8
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-05-11 16:16:35 +1000

    Changed icons in the in game menu to match the ones on the UI bar. Also looked into drawing lines in Unity, apparently the GL class which offers an interface to OpenGL methods is the way to go due to problems with other things Unity provides. However, they don't let you change the line width! So maybe we will have to do quads to represent a line instead? Anyway, thats all in the DrawLines script which is attached to main camera (another Unity requirement).
    Signed-off-by: JaydenIvanovic <s3331571@student.rmit.edu.au>

---
+++ HellaPlants/Assets/Scripts/DrawLine.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This has to be attached to the main camera to actually work...
+// YOU CANT CHANGE LINE WIDTH WITH GL LINE WIDTH!
+public class DrawLine : MonoBehaviour 
+{
+	public Material material;
+	public float xOrigin, yOrigin; // Where you want the shape to begin being drawn from.
+	private WizardPowerups wizard;
+
+	// Use this for initialization
+	void Start () 
+	{
+		wizard = GameObject.Find("Environment").GetComponent<WizardPowerups>();
+	}
+	
+	// Update is called once per frame
+	void OnPostRender () 
+	{
+		if(wizard.Exists())
+		{
+			GL.PushMatrix();
+				material.SetPass(0);
+				GL.Color(Color.black);
+				NorthEastLine(xOrigin, yOrigin);
+			GL.PopMatrix();
+		}
+	}
+	
+	private void NorthEastLine(float x, float y)
+	{
+		GL.Begin(GL.LINES);
+			GL.Vertex3(x, y, -5f);
+			GL.Vertex3(x + 3, yOrigin + 3, -5f);
+		GL.End();
+	}
+
+	private void NorthWestLine()
+	{
+
+	}
+
+	private void SouthEastLine()
+	{
+
+	}
+
+	private void SouthWestLine()
+	{
+
+	}
+}
