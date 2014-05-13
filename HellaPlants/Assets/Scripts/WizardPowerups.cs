3a5b483b73a0c5b8825d51ff89a3d05855c99819
s3322445 <s3322445@student.rmit.edu.au>
2014-05-13 14:33:39 +1000

    Added basic gesture showing. Currently it just displays each direction in a list, needs much prettifying etc.

--- HellaPlants/Assets/Scripts/WizardPowerups.cs
+++ HellaPlants/Assets/Scripts/WizardPowerups.cs
@@ -2,57 +2,61 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class WizardPowerups : MonoBehaviour 
+public class WizardPowerups : MonoBehaviour
 {
-	private bool wizardExists;
-	private float chance;
-	//public enum Powerups {Regeneration, SlowTime, KillBugs, Shield, None};
-	//private Powerups currentPowerup;
+    private bool wizardExists;
+    private float chance;
     private GestureMap.Spell currentPowerup;
-	public GameObject wizardPrefab;
-	private GameObject wizard;
+    public GameObject wizardPrefab;
+    private GameObject wizard;
     public GameObject player;
     private Spells spells;
-    //private GestureMap gestureMap;
+    public Texture NEarrowTexture;
+    public Texture NWarrowTexture;
+    public Texture SEarrowTexture;
+    public Texture SWarrowTexture;
     List<Gestures.direction> currentGesture;
 
-	// Use this for initialization
-	void Start () 
-	{
-        //player = GameObject.FindGameObjectWithTag("Player");
+    // Use this for initialization
+    void Start()
+    {
         spells = player.GetComponent<Spells>();
         currentGesture = null;
-        //gestureMap = spells.getGestureMap();
-		wizardExists = false;
-		currentPowerup = GestureMap.Spell.None;
-		chance = 0.0f;
-		InvokeRepeating ("RandomPowerup", 0f, 5f);
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	}
+        wizardExists = false;
+        currentPowerup = GestureMap.Spell.None;
+        chance = 0.0f;
+        InvokeRepeating("RandomPowerup", 0f, 5f);
+    }
 
-	// The wizard exists.
-	public bool Exists()
-	{
-		return wizardExists;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
-	private void ShowWizard()
-	{
+    void OnGUI()
+    {
+        if (Exists())
+            drawGesture();
+    }
+
+    public bool Exists()
+    {
+        return wizardExists;
+    }
+
+    private void ShowWizard()
+    {
         if (currentGesture != null)
         {
             Invoke("DestroyWizard", 15f);
-            spells.getGestureMap().SetPowerupGesture(currentPowerup,currentGesture);
+            spells.getGestureMap().SetPowerupGesture(currentPowerup, currentGesture);
             wizardExists = true;
             wizard = Instantiate(wizardPrefab) as GameObject;
         }
-	}
+    }
 
-	public void DestroyWizard()
-	{
+    public void DestroyWizard()
+    {
         if (wizardExists == true)
         {
             spells.getGestureMap().UnsetPowerupGesture(currentGesture);
@@ -60,121 +64,142 @@ public class WizardPowerups : MonoBehaviour
             wizardExists = false;
             Destroy(wizard);
         }
-	}
+    }
 
-	// Set current powerup to a random spell
-	private void SetRandomSpell()
-	{
-		int r = Random.Range (0, 4);
-		switch(r)
-		{
-			case 0:
+    private void drawGesture()
+    {
+        for (int i = 0; i < currentGesture.Count; i++)
+        {
+            Texture tex;
+            if (currentGesture[i] == Gestures.direction.NE)
+                tex = NEarrowTexture;
+            else if (currentGesture[i] == Gestures.direction.NW)
+                tex = NWarrowTexture;
+            else if (currentGesture[i] == Gestures.direction.SE)
+                tex = SEarrowTexture;
+            else if (currentGesture[i] == Gestures.direction.SW)
+                tex = SWarrowTexture;
+            else
+                return;
+
+            GUI.DrawTexture(new Rect(200 + (i*80), 200, 40, 40), tex, ScaleMode.ScaleAndCrop, true, 0);
+        }
+    }
+
+    // Set current powerup to a random spell
+    private void SetRandomSpell()
+    {
+        int r = Random.Range(0, 4);
+        switch (r)
+        {
+            case 0:
                 currentPowerup = GestureMap.Spell.Regeneration;
                 currentGesture = GetRandGesture(5);
-				break;
-			case 1:
+                break;
+            case 1:
                 currentPowerup = GestureMap.Spell.SlowTime;
                 currentGesture = GetRandGesture(4);
-				break;
-			case 2:
+                break;
+            case 2:
                 currentPowerup = GestureMap.Spell.KillBugs;
                 currentGesture = GetRandGesture(3);
-				break;
-			case 3:
+                break;
+            case 3:
                 currentPowerup = GestureMap.Spell.Shield;
                 currentGesture = GetRandGesture(2);
-				break;
-		}
-	}
+                break;
+        }
+    }
 
-	// Get new random powerup.
-	private void RandomPowerup()
-	{
-		if (wizardExists)
-			return;
+    // Get new random powerup.
+    private void RandomPowerup()
+    {
+        if (wizardExists)
+            return;
 
-		// success
-		if( chance > Random.Range(0, 1) )
-		{
-			// Choose a random spell.
-			SetRandomSpell();
-			ShowWizard();
-			chance = 0.0f;
-		}
+        // success
+        if (chance > Random.Range(0, 1))
+        {
+            // Choose a random spell.
+            SetRandomSpell();
+            ShowWizard();
+            chance = 0.0f;
+        }
         //failure
-		else
-			chance += 0.05f;
-	}
+        else
+            chance += 0.05f;
+    }
 
-	public Gestures.direction RandomDirection(Gestures.direction dir)
-	{
-		int r;
+    public Gestures.direction RandomDirection(Gestures.direction dir)
+    {
+        int r;
 
-		switch(dir)
-		{
-			case Gestures.direction.NE:
-				r = Random.Range (0, 2);
-				switch(r)
-				{
-					case 0:
-						return Gestures.direction.SE;
-					case 1:
-						return Gestures.direction.NW;
-				}
-				break;
-			case Gestures.direction.NW:
-				r = Random.Range (0, 2);
-				switch(r)
-				{
-					case 0:
-						return Gestures.direction.NE;
-					case 1:
-						return Gestures.direction.SW;
-				}
-				break;
-			case Gestures.direction.SE:
-				r = Random.Range (0, 2);
-				switch(r)
-				{
-					case 0:
-						return Gestures.direction.NE;
-					case 1:
-						return Gestures.direction.SW;
-				}
-				break;
-			case Gestures.direction.SW:
-				r = Random.Range (0, 2);
-				switch(r)
-				{
-					case 0:
-						return Gestures.direction.NW;
-					case 1:
-						return Gestures.direction.SE;
-				}
-				break;
-			case Gestures.direction.NONE:
-				r = Random.Range (0, 4);
-				switch(r)
-				{
-					case 0:
-						return Gestures.direction.NE;
-					case 1:
-						return Gestures.direction.NW;
-					case 2:
-						return Gestures.direction.SE;
-					case 3: 
-						return Gestures.direction.SW;
-				}
-				break;
-		}
+        switch (dir)
+        {
+            case Gestures.direction.NE:
+                r = Random.Range(0, 2);
+                switch (r)
+                {
+                    case 0:
+                        return Gestures.direction.SE;
+                    case 1:
+                        return Gestures.direction.NW;
+                }
+                break;
+            case Gestures.direction.NW:
+                r = Random.Range(0, 2);
+                switch (r)
+                {
+                    case 0:
+                        return Gestures.direction.NE;
+                    case 1:
+                        return Gestures.direction.SW;
+                }
+                break;
+            case Gestures.direction.SE:
+                r = Random.Range(0, 2);
+                switch (r)
+                {
+                    case 0:
+                        return Gestures.direction.NE;
+                    case 1:
+                        return Gestures.direction.SW;
+                }
+                break;
+            case Gestures.direction.SW:
+                r = Random.Range(0, 2);
+                switch (r)
+                {
+                    case 0:
+                        return Gestures.direction.NW;
+                    case 1:
+                        return Gestures.direction.SE;
+                }
+                break;
+            case Gestures.direction.NONE:
+                r = Random.Range(0, 4);
+                switch (r)
+                {
+                    case 0:
+                        return Gestures.direction.NE;
+                    case 1:
+                        return Gestures.direction.NW;
+                    case 2:
+                        return Gestures.direction.SE;
+                    case 3:
+                        return Gestures.direction.SW;
+                }
+                break;
+        }
 
-		return Gestures.direction.NONE;
-	}
+        return Gestures.direction.NONE;
+    }
 
-	public List<Gestures.direction> GetRandGesture (int length)
-	{
+    public List<Gestures.direction> GetRandGesture(int length)
+    {
         List<Gestures.direction> gesture = new List<Gestures.direction>();
-        do{
+        do
+        {
             gesture.Clear();
             gesture.Add(RandomDirection(Gestures.direction.NONE));
 
@@ -190,7 +215,7 @@ public class WizardPowerups : MonoBehaviour
                 directions += "Shield: ";
             if (currentPowerup == GestureMap.Spell.SlowTime)
                 directions += "Slow Time: ";
-            
+
             for (int i = 0; i < gesture.Count; i++)
             {
                 if (gesture[i] == Gestures.direction.NE)
@@ -205,6 +230,6 @@ public class WizardPowerups : MonoBehaviour
             Debug.Log(directions);
         } while (spells.getGestureMap().CheckGestureExists(gesture) == true); //Check the gesture isn't already in use
 
-		return gesture;
-	}
+        return gesture;
+    }
 }
