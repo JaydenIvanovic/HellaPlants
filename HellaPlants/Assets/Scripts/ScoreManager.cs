1b75f724fa84601460b11a0f74095a4327c852d3
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-05-17 14:01:29 +1000

    Got the scoreboard scene. Now we just need to record player scores and fill it. I'll probably finish this later.
    Signed-off-by: JaydenIvanovic <s3331571@student.rmit.edu.au>

--- HellaPlants/Assets/Scripts/ScoreManager.cs
+++ HellaPlants/Assets/Scripts/ScoreManager.cs
@@ -9,9 +9,9 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class ScoreManager : MonoBehaviour 
 {
 	private static ScoreManager scoreManager;
-	private const uint MAX_NUM = 10;
+	private const uint MAX_NUM = 5;
 	private string filePath = Application.persistentDataPath +  "/scores.dat";
-	private float[] scores;
+	private float[] scores; // Sorted from highest to lowest.
 
 	// Use this for initialization
 	void Start () 
@@ -23,6 +23,48 @@ public class ScoreManager : MonoBehaviour
 			scoreManager = this;
 		else if (scoreManager != this)
 			Destroy(gameObject);
+	}
+	
+	// Initialize all scores to zero.
+	private void InitScores()
+	{
+		for(int i = 0; i < MAX_NUM; ++i)
+			scores[i] = 0;
+	}
+	
+	// The score is higher than the lowest one recorded,
+	// or there exists space for additional scores.
+	public bool IsHighScore(float score)
+	{
+		if( scores[MAX_NUM - 1] < score )
+			return true;
+		return false;
+	}
+
+	// Add a new high score to the board.
+	// This calls IsHighScore() for insurance that
+	// the new score to be added is actually a high score.
+	public void AddScore(float score)
+	{
+		if( IsHighScore(score) )
+		{
+			// Find the insertion point. As there is only a small
+			// number of recorded scores, O(n) search is fine.
+			for(int i = 0; i < MAX_NUM; ++i)
+			{
+				if(scores[i] < score)
+				{
+					// Move all the scores starting at the score which is
+					// to be replaced down by 1.
+					for(int j = i; j < MAX_NUM - 1; ++j)
+						scores[j + 1] = scores[j];
+					// Put the new score into the appropriate position.
+					scores[i] = score;
+					// Finished necessary logic.
+					return; 
+				}
+			}
+		}
 	}
 
 	// Save the current scores to file.
@@ -51,6 +93,8 @@ public class ScoreManager : MonoBehaviour
 			// Put the data back into the array.
 			scores = scoreData.GetScores().Clone() as float[];
 		}
+		else
+			InitScores();
 	}
 	
 	// Extending a MonoBehaviour can cause errors when being read/written
